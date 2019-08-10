a409e49ec74ee40f21063317e72c10fcc42c5fec
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-10 16:03:04 -0400

    search_bar component is now a controlled component, input triggers setState()

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ""
+    }
+  }
   handleUpdate = (event) => {
     this.props.searchFunction(event.target.value)
+    // change the state
+    this.setState({
+      term: event.target.value
+    });
   }
   render() {
     return (
-      <input type="text" placeholder="search for a gif" className="form-control form-search"
+      <input type="text"
+        value={this.state.term}
+        placeholder="search for a gif"
+        className="form-control form-search"
         onChange={this.handleUpdate}
       />
     );
