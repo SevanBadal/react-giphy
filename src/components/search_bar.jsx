385bba4f6799127ee0d52f2fbf72d857d39bd9e4
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-10 13:46:10 -0400

    placeholder text added to search input

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -6,7 +6,7 @@ class SearchBar extends Component {
   }
   render() {
     return (
-      <input type="text" className="form-control form-search"
+      <input type="text" placeholder="search for a gif" className="form-control form-search"
         onChange={this.handleUpdate}
       />
     );
