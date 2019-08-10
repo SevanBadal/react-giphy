bda4a8b34b13e147a5b928886877ce634c16bea9
Sevan Golnazarian <sgolnaza@gmail.com>
2019-08-10 16:09:46 -0400

    deconstructed prop

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Gif from './gif.jsx';
 
-const GifList = (props) => {
+// prop destructuring
+const GifList = ({ gifs, selectGif }) => {
   return (
     <div className="gif-list">
-      {props.gifs.map(gif => <Gif id={gif.id} key={gif.id} selectGif={props.selectGif} />)}
+      {gifs.map(gif => <Gif id={gif.id} key={gif.id} selectGif={selectGif} />)}
     </div>
   );
 };
